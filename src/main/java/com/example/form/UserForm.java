aa1b2a669ffbd603cafca8585699181fd43cf747
OgawaShinji <shinji.ogawa@rakus-partners.co.jp>
2020-06-16 12:54:55 +0900

    create 04controller and 04.html

---
+++ src/main/java/com/example/form/UserForm.java
@@ -0,0 +1,37 @@
+package com.example.form;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+public class UserForm {
+
+	@NotBlank(message="名前は必須です")
+	private String name;
+	
+	@NotNull(message="値を入力してください")
+	private String age;
+	
+	@NotBlank(message="値を入力してください")
+	private String comment;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public String getComment() {
+		return comment;
+	}
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	
+}
